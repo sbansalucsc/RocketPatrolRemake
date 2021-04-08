949c02fcbca63a9c26cf305617caf3acd67dfa17
Shaurya Bansal <sbansal2@ucsc.edu>
2021-04-08 13:37:01 +0530

    Showing static starfield background

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,7 +5,15 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload() {
+        this.load.image('starfield', 'assets/starfield.png');
+    }
+
     create() {
+
+        this.starfield = this.add.tileSprite(0,0,
+            640,480, 'starfield').setOrigin(0,0);
+        
         // green UI background
         this.add.rectangle(0, 
             borderUISize + borderPadding, 
@@ -18,6 +26,8 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+
+        
 
 
         }
