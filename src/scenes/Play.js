a67aedf484a3ccad1afb97ac2fbc8dde9909df75
Shaurya Bansal <sbansal2@ucsc.edu>
2021-04-18 13:54:18 +0530

    Added custom enemy ships and rocket art

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,8 +7,8 @@ class Play extends Phaser.Scene {
 
     preload() {
         this.load.image('starfield', 'assets/starfield.png');
-        this.load.image('rocket', 'assets/rocket.png');
-        this.load.image('spaceship', 'assets/spaceship.png');
+        this.load.image('rocket', 'assets/SpaceRocket.png');
+        this.load.image('spaceship', 'assets/Space_Ship.png');
         this.load.image('bonusship', 'assets/BonusPixelShip.png');
         this.load.spritesheet('explosion', './assets/explosion.png', 
         {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
